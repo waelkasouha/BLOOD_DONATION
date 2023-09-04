f32904a0be2b430f1dcb4de2509f4c158743c772
wael kasouha <waelkas99@gmail.com>
2023-09-03 21:43:40 -0400

    edit user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('user_role', ['admin', 'user'])->default('user');
+            $table->enum('user_type', ['user', 'organization'])->default('user');
+            $table->enum('blood_type', ['A-', 'A+', 'B-', 'B+', 'AB-', 'AB+', 'O-', 'O+'])->default(null);
+            $table->enum('province', ["Aleppo","Damascus","Daraa","Deir ez-Zor","Hama","Hasakah","Homs","Idlib","Latakia","Quneitra","Raqqa","Rif Dimashq","Tartus","Suwayda"])->default(null);
+            $table->text('location')->default(null);
+            $table->string('phone_number')->default(null);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
